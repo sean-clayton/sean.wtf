e57362a63a340865ba507a8558b011bd5da1cc5b
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2019-06-07 18:50:33 -0400

    Fix seo title shtuff

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 module.exports = {
   pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   siteTitle: "<sean.wtf>", // Navigation and Site Title
-  siteTitleAlt: "<sean.wtf>", // Alternative Site title for SEO
-  siteTitleManifest: "<sean.wtf>",
+  siteTitleAlt: "sean.wtf", // Alternative Site title for SEO
+  siteTitleManifest: "sean.wtf",
   siteUrl: "https://www.sean.wtf", // Domain of your site. No trailing slash!
   siteLanguage: "en", // Language Tag on <html> element
-  siteHeadline: "<sean.wtf>", // Headline for schema.org JSONLD
+  siteHeadline: "sean.wtf", // Headline for schema.org JSONLD
   siteBanner: "/social/banner.png", // Your image for og:image tag. You can find it in the /static folder
   favicon: "src/favicon.png", // Your image for favicons. You can find it in the /src folder
   siteDescription: "Writings and such.", // Your site description
