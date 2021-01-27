7ceeeb76e5db24bd084c30902b1d35cae4ee287d
Sean Clayton <sean@seanclayton.me>
2021-01-27 16:36:53 -0500

    Add absolute url asset paths

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const withTM = require("next-transpile-modules");
 
-module.exports = withTM(["lodash-es"]);
+module.exports = withTM(["lodash-es"])({
+  assetPrefix: isProd ? "https://www.sean.wtf" : "",
+});
