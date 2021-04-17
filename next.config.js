bba0a931d1d35a68a2e635d814bb118cb031efc9
Sean Clayton <sean@seanclayton.me>
2021-04-17 16:11:26 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const withTM = require("next-transpile-modules");
 
 module.exports = withTM(["lodash-es"])({
+  assetPrefix: isProd ? "https://www.sean.wtf" : "",
   trailingSlash: true,
   exportPathMap: function () {
     return {
