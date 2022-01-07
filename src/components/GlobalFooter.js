32842b1f095e65355c80456c289f2e298a63beba
🎷🐢 Sean-Patrick Ortencio Clayton <sean@seanclayton.me>
2022-01-07 12:53:15 -0500

    Use the Free Public License

--- src/components/GlobalFooter.js
+++ src/components/GlobalFooter.js
@@ -6,12 +6,12 @@ export default function GlobalFooter() {
       <div className="mx-auto max-w-screen-md text-center">
         <div className="prose prose-sm prose-yellow max-w-none">
           All writings on this site by Sean Clayton are available under the{" "}
-          <Link href="/WTFPL-69.420">
-            <a>WTFPL-69.420</a>
+          <Link href="/fpl">
+            <a>Free Public License</a>
           </Link>
-          &nbsp;license.
+          .
           <br />
-          <a href="/WTFPL-69.420.txt">You can view the license here.</a>
+          <a href="/fpl.txt">You can view the license here.</a>
           <br />
           <a href="https://github.com/sean-clayton/sean.wtf">Git Repo</a>
         </div>
