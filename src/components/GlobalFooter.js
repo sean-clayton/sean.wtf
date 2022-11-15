d0ccc0436026518b96c9c31125cc627115428b84
🎷🐢 S. P. O. Clayton <yo@sean.wtf>
2022-11-15 10:34:27 -0500

    Add PGP public key

--- src/components/GlobalFooter.js
+++ src/components/GlobalFooter.js
@@ -5,15 +5,26 @@ export default function GlobalFooter() {
     <footer className="max-w-screen my-8">
       <div className="mx-auto max-w-screen-md text-center">
         <div className="prose prose-sm px-4 prose-yellow max-w-none">
-          All writings on this site by Sean Clayton are available under the{" "}
-          <Link href="/fpl">
-            <a>Free Public License</a>
-          </Link>
-          .
-          <br />
-          <a href="/fpl.txt">You can view the license here.</a>
-          <br />
-          <a href="https://github.com/sean-clayton/sean.wtf">Git Repo</a>
+          <div>
+            <a href="/pgp-public-key.txt">My PGP Public Key</a>
+          </div>
+          <div>
+            Anyone found copying the content in this website without permission
+            will be a real good friend of mine.
+          </div>
+          <div>
+            For those who falsely believe permission is necessary, you can rest
+            assured as this website is available under the{" "}
+            <Link href="/fpl">
+              <a>Free Public License</a>
+            </Link>
+            .
+            <br />
+            <a href="/fpl.txt">You can view the license here.</a>
+          </div>
+          <div>
+            <a href="https://github.com/sean-clayton/sean.wtf">Git Repo</a>
+          </div>
         </div>
       </div>
     </footer>
